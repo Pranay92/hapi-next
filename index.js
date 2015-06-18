9cf3d11972233e48688ce521659a536cdf3b5777
Pranay <pranay@Pranays-Mac-Mini-2.local>
2015-06-18 18:49:33 +0530

    Add promise style invocation of functions

--- index.js
+++ index.js
@@ -54,4 +54,50 @@ Series.prototype.execute = function(request,reply) {
 	});
 };
 
+Series.prototype.promise = function(request,reply) {
+
+	if(!request) {
+		throw new Error('Request can\'t be empty.');
+		return;
+	}
+
+	if(!reply) {
+		throw new Error('Reply can\'t be empty.');
+		return;
+	}
+
+	var arr = this.arr,
+			currCall;
+
+	async.series(arr.map(function(func) {
+
+		return function(cb) {
+
+			currCall = func.call({},request,reply);
+
+			if(!currCall || !currCall.then || (typeof currCall.then !== 'function') ) {
+				return cb();
+			}
+
+			currCall.then(function() {
+				cb();
+			})
+			.catch(function(e) {
+				cb(e);
+			});
+
+		}
+	}),function(err,results) {
+		
+		if(err) {
+			reply(boom.badData(err));
+		}
+
+		reply.data = reply.data || defaultData;
+		reply(reply.data);
+		
+	});
+
+};
+
 module.exports = Series;
